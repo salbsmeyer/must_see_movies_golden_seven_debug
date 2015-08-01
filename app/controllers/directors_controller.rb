b425be75b0d65f013bf5544ffce9f65e4e06b531
salbsmeyer <sophiealbsmeyer@Sophies-MacBook-Pro.local>
2015-08-01 16:12:07 -0500

    Movies_Edit

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -1,9 +1,9 @@
 class DirectorsController < ApplicationController
   def index
-    directors = Director.all
+    @directors = Director.all
   end
 
-  def show
+  def show_details
     @director = Director.find(params[:id])
   end
 
@@ -19,7 +19,7 @@ class DirectorsController < ApplicationController
 
     @director.save
 
-    render("show")
+    render("show_details")
   end
 
   def edit_form
@@ -28,15 +28,14 @@ class DirectorsController < ApplicationController
 
   def update_row
     @director = Director.find(params[:id])
-
-    @director.dob = params[":dob"]
-    @director.name = params[":name"]
-    @director.bio = params[":bio"]
-    @director.image_url = params[":image_url"]
+    @director.dob = params[:dob]
+    @director.name = params[:name]
+    @director.bio = params[:bio]
+    @director.image_url = params[:image_url]
 
     @director.save
 
-    render("show")
+    render("show_details")
   end
 
   def destroy
--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,22 +1,23 @@
 class MoviesController < ApplicationController
   def index
-    @movies = Movie.all
+    @movie = Movie.all
   end
 
   def show
-    @movie = Movie.find(params[id])
+    @movie = Movie.find(params["id"])
   end
 
   def new_form
   end
 
   def create_row
+    @movie = Movie.new
     @movie.title = params[:the_title]
     @movie.year = params[:the_year]
     @movie.duration = params[:the_duration]
     @movie.description = params[:the_description]
     @movie.image_url = params[:the_image_url]
-    @movie.director_id = params[:the_director_id]
+    @movie.director_id = params[:director_id]
 
     @movie.save
 
@@ -28,19 +29,23 @@ class MoviesController < ApplicationController
   end
 
   def update_row
-    @movie.title = params[:title]
-    @movie.year = params[:year]
-    @movie.duration = params[:duration]
-    @movie.description = params[:description]
-    @movie.image_url = params[:image_url]
-    @movie.director_id = params[:director_id]
+    @movie = Movie.find(params["id"])
+    @movie.title = params["title"]
+    @movie.year = params["year"]
+    @movie.duration = params["duration"]
+    @movie.description = params["description"]
+    @movie.image_url = params["image_url"]
+    @movie.director_id = params["director_id"]
+
+    @movie.save
 
     render("show")
   end
 
-  def destroy
-    movie = Movie.find(params[:id])
 
-    movie.destroy
+  def destroy
+    @movie = Movie.find(params[:id])
+
+    @movie.destroy
   end
 end
